115d40d2869f9ddad721be484d2444b9275f09e9
WasabiPingKak <wasabi.pingkak@gmail.com>
2025-04-13 18:36:08 +0800

    Add a mock data to display categories and tags in frontend

---
+++ frontend_react/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ClassificationEditorMock from "./ClassificationEditorMock";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ClassificationEditorMock />
+  </React.StrictMode>
+);
