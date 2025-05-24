1ded7a3bc7533f9a6395a36554ee4dd81637916c
WasabiPingKak <wasabi.pingkak@gmail.com>
2025-05-24 19:38:53 +0800

    Add Google Analytics, OAuth verified

--- frontend_react/src/components/common/SidebarMenu.jsx
+++ frontend_react/src/components/common/SidebarMenu.jsx
@@ -91,7 +91,7 @@ const SidebarMenu = ({ collapsed, setCollapsed, isMobile = false, onItemClick })
                         className="flex items-center gap-3 px-3 py-2 rounded text-green-700 dark:text-green-300 hover:bg-green-50 dark:hover:bg-zinc-800"
                     >
                         <span>🔗</span>
-                        {!collapsed && <span>授權我的頻道(審核中)</span>}
+                        {!collapsed && <span>授權我的頻道</span>}
                     </button>
 
                     <a
